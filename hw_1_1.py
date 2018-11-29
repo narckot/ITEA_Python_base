9ed95bb799ab7464463347550938769f9cf4f022
narckot <45234683+narckot@users.noreply.github.com>
2018-11-29 18:17:06 +0200

    Update and rename HT_1.py to hw_1_1.py

---
+++ hw_1_1.py
@@ -0,0 +1,12 @@
+name = (input('Введите имя и фамалию: '))
+
+print('Здравствуй ', name, '!')
+
+b_day = input('Введите Ваш день даты рождения.: ')
+month = int(input('Введите Ваш месяц даты рождения: '))
+year = int(input('Введите Ваш год даты рождения: '))
+year_old = int(input('Введите Ваш возраст в годах:'))
+
+year_sum = print(int(year_old * 12 + month))
+
+d = print(int(year_old * 360 + month * 30 + b_day))
