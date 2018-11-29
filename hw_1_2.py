8f9ef4eacda03131f649408036fcbe70fe88da67
narckot <45234683+narckot@users.noreply.github.com>
2018-11-29 17:20:51 +0200

    hw_1_2

---
+++ hw_1_2.py
@@ -0,0 +1,10 @@
+first_num = float(input('введите перове число: '))
+second_num = float(input('введите второе число: '))
+
+print(first_num + second_num)  # сумма
+print(first_num - second_num)  # разница
+print(first_num * second_num)  # умножение
+print(first_num / second_num)  # деление
+print(first_num // second_num)  # Целочисленное деление
+print(first_num ** second_num)  # Возводим число в степень
+print(first_num % second_num)  # Получение остатка от деления
